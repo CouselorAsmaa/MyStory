83e012dca35905f4534905ad2162c23661d2ecc3
CouselorAsmaa <asmi99@windowslive.com>
2021-12-09 17:42:39 +0300

    navigation drawer completed

--- app/src/main/java/com/example/mystory/MainActivity.kt
+++ app/src/main/java/com/example/mystory/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.mystory
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private var textViewEmail:TextView? = null
+    private var textViewEmail: TextView? = null
+    private var drawerLayout: DrawerLayout? = null
+    private var toolbarView: Toolbar? = null
+    private var navigationView:NavigationView?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +28,60 @@ class MainActivity : AppCompatActivity() {
         connectViews()
 
         textViewEmail?.text = email
+        setSupportActionBar(toolbarView)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        setupDrawer()
+        updateEmailInHeader(email!!)
+        drawerClicks()
+
+    }
+    private fun updateEmailInHeader(email:String){
+        val haederView = navigationView?.getHeaderView(0)
+        val textViewEmail = haederView?.findViewById<TextView>(R.id.tvEmail)
+        textViewEmail?.text = email
+    }
+    private fun setupDrawer(){
+
+        val toggle = ActionBarDrawerToggle(this,drawerLayout
+            ,R.string.open,R.string.close)
+        drawerLayout?.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            android.R.id.home ->{
+                drawerLayout?.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> true
+
+        }
     }
     private fun connectViews(){
         textViewEmail = findViewById(R.id.tvEmail)
+        drawerLayout = findViewById(R.id.drawer)
+        toolbarView = findViewById(R.id.toolbar)
+        navigationView = findViewById(R.id.navView)
+
+    }
+    private fun drawerClicks(){
+        navigationView?.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home ->{
+                    drawerLayout?.closeDrawer(GravityCompat.START)
+                    true
+                }
+                R.id.logout ->{
+                    finish()
+                    val i = Intent(this,LoginActivity::class.java)
+                    startActivity(i)
+
+                    true
+                }
+                else -> true
+            }
+        }
     }
 }
